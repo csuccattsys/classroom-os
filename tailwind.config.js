66eeaeed5a64298ea07eb007ba3a3fc341e855cb
csuccatt.sys <davie.sialongo@csucc.edu.ph>
2026-06-05 16:11:02 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- Crucial: This scans all sub-folders inside src
   ],
   theme: {
     extend: {},
